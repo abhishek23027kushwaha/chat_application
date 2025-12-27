28ec3b71baabb5fd4e8e317029520d96fd33b436
abhishek23027kushwaha <abhishek8888raj@gmail.com>
2025-12-27 08:59:23 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDb from "./config/db.js"
+import authRouter from "./routes/auth.routes.js"
+import cookieParser from "cookie-parser"
+import path from "path"
+dotenv.config({ path: "./backend/.env" });
+import cors from "cors"
+import userRouter from "./routes/user.routes.js"
+import messageRouter from "./routes/message.routes.js"
+import { app, server } from "./socket/socket.js"
+const _dirname = path.resolve();
+const port=process.env.PORT || 5000
+
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api/auth",authRouter)
+app.use("/api/user",userRouter)
+app.use("/api/message",messageRouter)
+app.use("/public", express.static("public"));
+
+
+
+// frontend
+app.use(express.static(path.join(_dirname, "frontend/dist")));
+
+app.use((req, res) => {
+  res.sendFile(
+    path.resolve(_dirname, "frontend", "dist", "index.html")
+  );
+});
+server.listen(port,()=>{
+    connectDb()
+    console.log("server started")
+})
